06da72efe4ac25ba9f120592230e1ec0d1a9d4f4
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-06-29 20:30:23 -0600

    Create rails app
    practice using route helpers, accepting ids as variables and using explicit and explicit names for routes

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,13 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+    @student.change_activity_status
+    redirect_to :action => "show"
   end
 
   private
